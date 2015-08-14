f0dafc2c46a9804a3231239d5d47ad6cb7842fce
Chris Whitworth <chris.whitworth@red-gate.com>
2015-08-14 13:54:09 +0100

    Draw sampled/unsampled curves in green/red, hide others

--- sampling.js
+++ sampling.js
@@ -4,28 +4,35 @@ var sampling = (function() {
         // Sampling?
         var idx = 0, success = 0, fail = 0, cont = 0, deltaAcc = 0.0;
         var THRESHOLD = 1 / (2*resolution);
-        var found = [];
         var sampledData = [];
         for(x = 0; x <= resolution; x++) {
             var xx = x / resolution;
 
+            var p = undefined;
             do {
                 var delta = data[idx].x - xx;
 
                 if (Math.abs(delta) < THRESHOLD) {
                     success ++; deltaAcc += Math.abs(delta);
-                    sampledData.push(Point(xx, data[idx].y));
+                    p = Point(xx, data[idx].y);
                     break;
                 }
 
-                if(delta > 0) {
+                if(delta >= 0) {
                     fail ++; deltaAcc += Math.abs(delta);
-                    sampledData.push(Point(xx, data[idx].y));
+                    p = Point(xx, data[idx].y);
                     break;
                 }
                 cont++;
                 idx++;
             } while (idx < resolution);
+
+            if (p === undefined) {
+                p = Point(xx, data[resolution].y);
+            }
+
+            sampledData.push(p);
+
         }
         return {
             sampledData: sampledData,
